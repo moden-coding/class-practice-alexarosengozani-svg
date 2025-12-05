4e40105bfdbc1a70a0fa7a7cf95e13f5ae5200b2
alexarosengozani-svg <alexarosengozani@gmail.com>
2025-12-05 13:49:41 -0500

    fer

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        Classroom robotics = new Classroom();
+        addperson(name);
     }
 }
